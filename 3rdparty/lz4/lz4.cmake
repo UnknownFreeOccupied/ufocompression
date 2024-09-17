96c313daed870460acf39c2fd4fba0ecf4cdd212
Daniel Duberg <danielduberg@gmail.com>
2024-09-17 10:53:26 +0200

    LZ4 1.10.0

--- 3rdparty/lz4/lz4.cmake
+++ 3rdparty/lz4/lz4.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
 	lz4
 	GIT_REPOSITORY	https://github.com/lz4/lz4.git
-	GIT_TAG 	      5ff839680134437dbf4678f3d0c7b371d84f4964 # v1.9.4
+	GIT_TAG 	      ebb370ca83af193212df4dcbadcc5d87bc0de2f0 # v1.10.0
 	GIT_PROGRESS    TRUE
 	SOURCE_SUBDIR		build/cmake
 )
