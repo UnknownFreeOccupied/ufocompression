5457ce6869610faa8fc975063273ec438b776fca
Daniel Duberg <danielduberg@gmail.com>
2024-11-14 10:19:29 +0100

    Upgraded from 2.2.0 to 2.2.2

--- 3rdparty/zlib/zlib.cmake
+++ 3rdparty/zlib/zlib.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
 	zlib
 	GIT_REPOSITORY	https://github.com/zlib-ng/zlib-ng.git
-	GIT_TAG 	      80514c17b384df68fbe83cca69ece0521b85f708 # 2.2.0
+	GIT_TAG 	      c939498c7f2a1d645833a98251365e2c814fd736 # 2.2.2
 	GIT_PROGRESS    TRUE
 )
 
